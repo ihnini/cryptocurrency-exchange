84226926e27fd9580076d3d8f90a4c1c6b420b78
unknown <ammar.kebiri@gmail.com>
2020-03-03 16:16:09 +0100

    version 1

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -14,18 +14,22 @@ public class CryptocurrencyBank {
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+        buyers.add(buyerCustomer);
         if(sellers.size() == 0) return 0;
-            for (Customer s : sellers){
-                if (s.getCryptocurrency().getType().equals(bitcoin))
-                    if(s.getCryptocurrency().getQte()>=i){
-                        s.setBalance(i);
-                        s.getCryptocurrency().setQte(s.getCryptocurrency().getQte()-i);
-                        buyerCustomer.setBalance(buyerCustomer.getBalance()-i);
-                        buyerCustomer.setCryptocurrency(new Cryptocurrency(bitcoin,i));
-                        return i;
-                    }
-                    }
-                return 0;
+        int price = 1;
+        if(buyers.size()>1)
+            price = (int)Math.pow(buyers.size(),2) - buyers.size();
+        for (Customer s : sellers){
+            if (s.getCryptocurrency().getType().equals(bitcoin))
+                if(s.getCryptocurrency().getQte()>=i){
+                    s.setBalance(s.getBalance()+i*price);
+                    s.getCryptocurrency().setQte(s.getCryptocurrency().getQte()-i);
+                    buyerCustomer.setBalance(buyerCustomer.getBalance()-i*price);
+                    buyerCustomer.setCryptocurrency(new Cryptocurrency(bitcoin,i));
+                    return i;
+                }
+                }
+            return 0;
     }
 
     public void addSeller(Customer sellerCustomer) {
