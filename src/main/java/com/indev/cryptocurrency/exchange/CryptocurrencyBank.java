b82122a67563f11d08d6f8b84fa9deaf7c67429c
simo4alaoui <simo4alaoui@gmail.com>
2020-03-03 15:08:14 +0100

    test 1 & 2

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,8 +1,26 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
+
+    private List<String> supportedCryptoCurrencies;
+
+    public List<String> getSupportedCryptoCurrencies() {
+        return supportedCryptoCurrencies;
+    }
+
+    public void setSupportedCryptoCurrencies(List<String> supportedCryptoCurrencies) {
+        this.supportedCryptoCurrencies = supportedCryptoCurrencies;
+    }
+
+    public CryptocurrencyBank() {
+        supportedCryptoCurrencies= new ArrayList<>();
+    }
+
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+        supportedCryptoCurrencies.add(bitcoin);
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,52 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
+
+    private String cryptocurrency;
+    private int wallet;
+    private int balance;
+
+    public Customer() {
+    }
+
+    public String getCryptocurrency() {
+        return cryptocurrency;
+    }
+
+    public void setCryptocurrency(String cryptocurrency) {
+        this.cryptocurrency = cryptocurrency;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return getBalance() + ":$," + getWallet() + ":" + getCryptocurrency();
+    }
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+        setCryptocurrency(bitcoin);
+        setWallet(i);
+        return this;
     }
 
     public Customer withBalance(int i) {
         return null;
     }
+
+
 }
