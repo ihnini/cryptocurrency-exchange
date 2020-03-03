b3c9d1bc32a856c6189b9a337b1ab98a7f1a8447
AhmedBenkiran-source <55805921+AhmedBenkiran-source@users.noreply.github.com>
2020-03-03 15:02:09 +0100

    commit requestTransaction

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,11 +1,21 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
+    List<String> listCryptoCurrency = new ArrayList<String>();
+    public CryptocurrencyBank(){
+
+    }
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+        listCryptoCurrency.add(bitcoin);
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+        buyerCustomer.setCurrency(bitcoin);
+        buyerCustomer.setQuentity(i);
         return 0;
     }
 
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,52 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
+    private String currency;
+    private int dollard;
+    private int quentity;
+
+    public Customer(String currency, int quentity) {
+        this.currency = currency;
+        this.quentity = quentity;
+    }
+    public Customer() {
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public int getQuentity() {
+        return quentity;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setQuentity(int quentity) {
+        this.quentity = quentity;
+    }
+
+    public void setDollard(int dollard) {
+        this.dollard = dollard;
+    }
+
+    public int getDollard() {
+        return dollard;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDollard()+":$,"+ this.getQuentity()+":"+this.getCurrency();
+    }
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+        return new Customer(bitcoin,i);
     }
 
     public Customer withBalance(int i) {
-        return null;
+        this.setDollard(i);
+        return this;
     }
 }
