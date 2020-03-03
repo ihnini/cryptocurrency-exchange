c60fe71fa02585260cf7abe0c3f6f2c8576b74d4
Hala <sebbah.hala@gmail.com>
2020-03-03 18:07:41 +0100

    first commit

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,44 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
+
+    List<String> SupportedCryptoCurrency ;
+    List<Customer> Sellers ;
+    int numberOfBuyers = 1;
+    int priceofSelling = 1;
+
+    public CryptocurrencyBank() {
+        SupportedCryptoCurrency = new ArrayList<String>();
+        Sellers = new ArrayList<Customer>();
+    }
+
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+        SupportedCryptoCurrency.add(bitcoin);
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-        return 0;
+        int boughtQuantity = 0;
+        if(numberOfBuyers>1) priceofSelling = numberOfBuyers*numberOfBuyers - numberOfBuyers;
+        for(Customer seller : Sellers){
+            if(i <= 0) break;
+            if(seller.bitcoin.equals(bitcoin) && seller.quantity > 0){
+                int actual_trans = (i<=seller.quantity) ? i : seller.quantity;
+                if(buyerCustomer.balance >= actual_trans*priceofSelling){
+                    seller.withCryptocurrency(bitcoin,-1* actual_trans).withBalance(actual_trans*priceofSelling);
+                    buyerCustomer.withCryptocurrency(bitcoin,actual_trans).withBalance(-1*actual_trans*priceofSelling);
+                    i -= actual_trans;
+                    boughtQuantity +=actual_trans;
+                }
+            }
+        }
+        numberOfBuyers++;
+        return boughtQuantity;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        Sellers.add(sellerCustomer);
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,28 @@
 package com.indev.cryptocurrency.exchange;
 
+
 public class Customer {
+
+    String bitcoin ;
+    int quantity = 0;
+    boolean hasCrypto = false;
+    int balance = 0;
+
     public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+        hasCrypto = true;
+        this.bitcoin = bitcoin;
+        this.quantity += i;
+        return this;
+    }
+    public Customer withBalance(int i){
+        this.balance += i;
+        return this;
     }
 
-    public Customer withBalance(int i) {
-        return null;
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder(balance+":$");
+        if(hasCrypto) res.append(","+quantity+":"+bitcoin);
+        return res.toString();
     }
 }
