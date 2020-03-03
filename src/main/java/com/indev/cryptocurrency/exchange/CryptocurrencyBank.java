20b21c720856813c3590d6b7407b748af6391c1d
YassineAlalou <yassine.alalou96@gmail.com>
2020-03-03 18:18:18 +0100

    Last Commit

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,39 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
+    List<Customer> listCustomer = new ArrayList<Customer>();
+    private int nb = 0;
+
+
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+
     }
 
     public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
+        nb ++ ;
+        int c = 1;
+        if(nb > 1){
+            c = (int) (Math.pow(nb,2)-nb);
+        }
+        for(Customer cus: listCustomer){
+            if(cus.getI() >= i && cus.getBitcoin().equals(bitcoin)){
+                cus.setI(cus.getI()-i);
+                cus.setDollar((cus.getDollar()+i*c));
+
+                buyerCustomer.setBitcoin("Bitcoin");
+                buyerCustomer.setI(buyerCustomer.getI()+i);
+                buyerCustomer.setDollar((buyerCustomer.getDollar()-i*c));
+                return i;
+            }
+        }
         return 0;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        listCustomer.add(sellerCustomer);
     }
+
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,56 @@
 package com.indev.cryptocurrency.exchange;
 
+
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+
+    private String bitcoin;
+    private int dollar = 0 ;
+    private int i ;
+
+    public Customer withCryptocurrency(String bitcoin,  int i) {
+
+        this.bitcoin = bitcoin;
+        // this.dollar = 0;
+        this.i = i;
+        return this;
+    }
+
+    public String getBitcoin() {
+        return bitcoin;
+    }
+
+    public int getDollar() {
+        return dollar;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+
+    public void setDollar(int dollar) {
+        this.dollar = dollar;
+    }
+
+    public void setBitcoin(String bitcoin) {
+        this.bitcoin = bitcoin;
+    }
+
+    public void setI(int i) {
+        this.i = i;
     }
 
     public Customer withBalance(int i) {
-        return null;
+        this.setDollar(i);
+        return this;
     }
+
+    @Override
+    public String toString() {
+        if(this.bitcoin != null){
+            return  dollar+":$," + i + ":" + bitcoin;
+        }
+        return dollar+":$" ;
+    }
+
 }
